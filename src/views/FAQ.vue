a1c942b7a99cba89438ab7c4287101f217185d03
FabianKostenzer <fabian.kostenzer@gmail.com>
2021-09-02 16:07:23 +0200

    feat: Added Impressum and FAQ

--- src/views/FAQ.vue
+++ src/views/FAQ.vue
@@ -1,6 +1,49 @@
 <template>
-  <section class="page" id="home">
-    <h1>FAQ</h1>
+  <section class="page scroll-fade" id="FAQ">
+    <h2>FAQ</h2>
+
+    <p class="question">Dies ist eine Frage?</p>
+    <p class="answer">
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur
+      veritatis, doloremque officiis ab eveniet aut molestias ducimus iste nihil
+      asperiores!
+    </p>
+    <p class="question">Dies ist eine Frage?</p>
+    <p class="answer">
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur
+      veritatis, doloremque officiis ab eveniet aut molestias ducimus iste nihil
+      asperiores!
+    </p>
+    <p class="question">Dies ist eine Frage?</p>
+    <p class="answer">
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur
+      veritatis, doloremque officiis ab eveniet aut molestias ducimus iste nihil
+      asperiores!
+    </p>
+    <p class="question">Dies ist eine Frage?</p>
+    <p class="answer">
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur
+      veritatis, doloremque officiis ab eveniet aut molestias ducimus iste nihil
+      asperiores!
+    </p>
+    <p class="question">Dies ist eine Frage?</p>
+    <p class="answer">
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur
+      veritatis, doloremque officiis ab eveniet aut molestias ducimus iste nihil
+      asperiores!
+    </p>
+    <p class="question">Dies ist eine Frage?</p>
+    <p class="answer">
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur
+      veritatis, doloremque officiis ab eveniet aut molestias ducimus iste nihil
+      asperiores!
+    </p>
+    <p class="question">Dies ist eine Frage?</p>
+    <p class="answer">
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur
+      veritatis, doloremque officiis ab eveniet aut molestias ducimus iste nihil
+      asperiores!
+    </p>
   </section>
 </template>
 
@@ -10,4 +53,17 @@ export default {
 }
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+#FAQ {
+  @include text-style(regular, medium, 23px);
+  padding-bottom: $whitespace-scroll;
+
+  .question {
+    margin: 30px 0 10px;
+  }
+  .answer {
+    font-weight: font-weight(regular);
+    opacity: 0.6;
+  }
+}
+</style>
--- src/views/Impressum.vue
+++ src/views/Impressum.vue
@@ -1,6 +1,62 @@
 <template>
-  <section class="page" id="home">
-    <h1>Impressum</h1>
+  <section class="page" id="Impressum">
+    <h2>Impressum</h2>
+    <p>
+      Informationspflicht laut §5 E-Commerce Gesetz, §14 Unternehmensgesetzbuch,
+      §63 Gewerbeordnung und Offenlegungspflicht laut §25 Mediengesetz.<br /><br />
+
+      Fabian Kostenzer<br />
+      FRAX 513<br />
+      6232 Münster<br />
+      Österreich<br /><br />
+
+      Tel.: 0650/3836370<br />
+      E-Mail: fabian.kostenzer@gmail.com<br /><br />
+
+      Matthias Bucher<br />
+      Rudolf Ostermann Weg 18c<br />
+      6175 Kematen<br />
+      Österreich<br /><br />
+
+      Tel.: 0664/3958767<br />
+      E-Mail: matthias.bucher@gmail.com<br /><br />
+
+      Quelle: Erstellt mit dem Impressum Generator von firmenwebseiten.at in
+      Kooperation mit pressefeuer.at<br /><br />
+
+      EU-Streitschlichtung<br />
+      Angaben zur Online-Streitbeilegung: Verbraucher haben die Möglichkeit,
+      Beschwerden an die OnlineStreitbeilegungsplattform der EU zu richten:
+      https://ec.europa.eu/consumers/odr/main/index.cfm?event=main.home2.show&lng=DE.
+      Sie können allfällige Beschwerde auch an die oben angegebene
+      E-Mail-Adresse richten.<br /><br />
+
+      Haftung für Inhalte dieser Webseite<br />
+      Ich entwickle die Inhalte dieser Webseite ständig weiter und bemühe mich
+      korrekte und aktuelle Informationen bereitzustellen. Leider kann ich keine
+      Haftung für die Korrektheit aller Inhalte auf dieser Webseite übernehmen,
+      speziell für jene die seitens Dritter bereitgestellt wird. Sollten Ihnen
+      problematische oder rechtswidrige Inhalte auffallen, bitte ich Sie mich
+      umgehend zu kontaktieren, Sie finden die Kontaktdaten im Impressum.<br /><br />
+
+      Haftung für Links auf dieser Webseite<br />
+      Meine Webseite enthält Links zu anderen Webseiten für deren Inhalt ich
+      nicht verantwortlich bin. Wenn Ihnen rechtswidrige Links auf unserer
+      Webseite auffallen, bitte ich Sie mich zu kontaktieren, Sie finden die
+      Kontaktdaten im Impressum.<br /><br />
+
+      Urheberrechtshinweis<br />
+      Alle Inhalte dieser Webseite (Bilder, Fotos, Texte, Videos) unterliegen
+      dem Urheberrecht. Falls notwendig, werde ich die unerlaubte Nutzung von
+      Teilen der Inhalte meiner Seite rechtlich verfolgen.<br /><br />
+
+      Bildernachweis<br />
+      Die Bilder, Fotos und Grafiken auf dieser Webseite sind urheberrechtlich
+      geschützt.<br /><br />
+
+      Die Bilderrechte liegen bei den folgenden Erstellern:<br />
+      Fabian Kostenzer
+    </p>
   </section>
 </template>
 
@@ -10,4 +66,14 @@ export default {
 }
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+#Impressum {
+  padding-bottom: $whitespace-scroll;
+
+  p {
+    @include text-style(regular, regular, 23px);
+    margin-top: 30px;
+    word-wrap: break-word;
+  }
+}
+</style>
